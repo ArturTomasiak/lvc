bd41ff9f187224e4cd536dc0a1e2b379cb6cdff6
ArturTomasiak <arturkrystiantomasiak@gmail.com>
2026-08-09 16:36:37 +0200

    API fix workspace::all_objects iterating through .lvc

--- api/src/core/workspace/all_objects.cpp
+++ api/src/core/workspace/all_objects.cpp
@@ -6,17 +6,28 @@ std::vector<Object> workspace::all_objects(std::filesystem::path working_directo
 
 try {
     std::filesystem::recursive_directory_iterator iterator(working_directory);
-     
-    for (const std::filesystem::directory_entry& entry : iterator) {
+    std::filesystem::recursive_directory_iterator end;
+
+    for (const std::filesystem::directory_entry& entry = *iterator; iterator != end; iterator++) {
+        if (entry.is_directory() &&
+            entry.path().filename() == ".lvc") {
+            iterator.disable_recursion_pending();
+            continue;
+        }
+
         if (!entry.is_regular_file())
             continue;
+
         Object object;
         object.path = entry.path().lexically_relative(working_directory);
         object.type = BLOB;
+
         std::string buffer = io::content(entry.path(), 0);
         insert_pattern(buffer, TYPE_BLOB);
+
         char id[65];
         sha256(buffer.data(), buffer.size(), id);
+
         object.id = id;
         out.push_back(std::move(object));
     }
